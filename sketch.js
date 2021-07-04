2752cfdd2532bebd601793a121d839a42fac31d9
teo523 <teodoro.dan@gmail.com>
2021-07-04 21:10:59 +0100

    added ctrlDiv

--- sketch.js
+++ sketch.js
@@ -160,7 +160,7 @@ function setup() {
 
 
     ctrlDiv = select("#ctrlDiv");
-    ctrlDiv.style("width",JSON.stringify(floor(leftDiv.width/2)));
+    ctrlDiv.style("width",JSON.stringify(floor(width*leftMargin/2)));
     ctrlDiv.style("height",JSON.stringify((height-cueHeight*height)/2));
     ctrlDiv.position(leftMargin*width/2-ctrlDiv.width/2,height/2 - height/20 - inp1.height );
     ctrlDiv.style("background-color","#transparent");
